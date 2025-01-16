2bfe5c7c478cde7defb7ccbe9b14b3f012025e31
Alex <raposoalexander@gmail.com>
2025-01-15 22:08:09 -0500

    Setup Spring API

---
+++ API/src/main/java/com/citycite_api/CityCiteAPI.java
@@ -0,0 +1,15 @@
+package com.citycite_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCiteAPI {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CityCiteAPI.class, args);
+
+	}
+
+}
