d8341d9f99fa45481e87a3cb1789cf805798da40
Alex <raposoalexander@gmail.com>
2025-01-23 13:51:46 -0500

    Modified JurisdictionService to not use validators ( should only occur in controller )

--- API/src/main/java/com/citycite_api/enforcement/controller/iJurisdictionController.java
+++ API/src/main/java/com/citycite_api/enforcement/controller/iJurisdictionController.java
@@ -1,14 +1,13 @@
 package com.citycite_api.enforcement.controller;
 
 import com.citycite_api.enforcement.dto.JurisdictionResponse;
-import com.citycite_api.enforcement.entity.Jurisdiction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.Set;
 
 @RestController
--- API/src/main/java/com/citycite_api/enforcement/service/iJurisdictionService.java
+++ API/src/main/java/com/citycite_api/enforcement/service/iJurisdictionService.java
@@ -3,13 +3,11 @@ package com.citycite_api.enforcement.service;
 import com.citycite_api.enforcement.dto.JurisdictionResponse;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Service
 @Transactional
 public interface iJurisdictionService {
     public Set<JurisdictionResponse> getAll();
-    public boolean isSupported(@Size(min = 2, max = 32) @NotNull String city, @Size(min = 2, max = 2) @NotNull String stateInitials);
+    public boolean isSupported(String city, String stateInitials);
 }
