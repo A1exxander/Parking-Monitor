8aa184b73154f6d3408d9487879b43db43f1d067
Alex <raposoalexander@gmail.com>
2025-01-23 09:53:29 -0500

    Fixed bug w iJurisdictionRepository - Integer was passed into first template arg & not the entity

--- API/src/main/java/enforcement/repository/iJurisdictionRepository.java
+++ API/src/main/java/enforcement/repository/iJurisdictionRepository.java
@@ -2,7 +2,9 @@ package enforcement.repository;
 
 import enforcement.entity.Jurisdiction;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface JurisdictionRepository extends CrudRepository<Integer, Jurisdiction> {
+public interface iJurisdictionRepository extends CrudRepository<Jurisdiction, Integer> {
+    public List<Jurisdiction> findAll();
     boolean existsByCityAndStateInitials(String city, String stateInitials);
 }
