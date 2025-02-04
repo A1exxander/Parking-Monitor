ff4004d49458483a0396c2b0602e892b0d116e46
Alex <raposoalexander@gmail.com>
2025-02-03 21:22:35 -0500

    Added method to report repository to find by report status, which is useful for populating redis cache of open tickets

--- API/src/main/java/com/citycite_api/report/repository/iReportRepository.java
+++ API/src/main/java/com/citycite_api/report/repository/iReportRepository.java
@@ -1,13 +1,33 @@
 package com.citycite_api.report.repository;
 
 import com.citycite_api.report.entity.Report;
+import com.citycite_api.report.entity.ReportStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface iReportRepository extends JpaRepository<Report, Integer> {
     Page<Report> findBySubmittingUser_ID(Pageable pageable, Integer userId);
     Page<Report> findByRespondingOfficer_ID(Pageable pageable, Integer officerId);
+    @Query(value = """
+        SELECT * FROM Report r
+        WHERE 
+            CASE
+                WHEN r.RespondingOfficerID IS NULL 
+                    AND r.CreatedAt >= NOW() - INTERVAL 2 HOUR 
+                    AND r.ResolutionStatus IS NULL THEN 'OPEN'
+                WHEN r.RespondingOfficerID IS NOT NULL 
+                    AND r.CreatedAt >= NOW() - INTERVAL 2 HOUR 
+                    AND r.ResolutionStatus IS NULL THEN 'IN_PROCESS'
+                WHEN r.CreatedAt < NOW() - INTERVAL 2 HOUR 
+                    AND r.ResolutionStatus IS NULL THEN 'TIMED_OUT'
+                WHEN r.ResolutionStatus IS NOT NULL THEN 'RESOLVED'
+            END = :#{#status.name()}
+    """, nativeQuery = true)
+    List<Report> findByReportStatus(@Param("status") ReportStatus status);
 }
