33f29bf11bdab8b74861662ec46d8f6a4b0e63c7
Alex <raposoalexander@gmail.com>
2025-02-06 23:24:22 -0500

    Added CTOR for CredentialsService

--- API/src/main/java/com/citycite_api/auth/service/CredentialsService.java
+++ API/src/main/java/com/citycite_api/auth/service/CredentialsService.java
@@ -7,9 +7,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Service
 @Transactional
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
 public class CredentialsService implements iCredentialsService {
 
     @Autowired
