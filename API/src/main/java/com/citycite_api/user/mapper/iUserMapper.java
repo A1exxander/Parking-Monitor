66383472aa7dcfb0d6eaff98a9d44115b22bfbac
Alex <raposoalexander@gmail.com>
2025-02-16 08:53:05 -0500

    Fixed usermapper to map birthdate properly

--- API/src/main/java/com/citycite_api/user/mapper/iUserMapper.java
+++ API/src/main/java/com/citycite_api/user/mapper/iUserMapper.java
@@ -3,8 +3,10 @@ package com.citycite_api.user.mapper;
 import com.citycite_api.user.dto.UserRequest;
 import com.citycite_api.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface iUserMapper {
+    @Mapping(target = "birthDate", source = "birthDate")
     public User userRequestToUser(UserRequest userRequest);
 }
