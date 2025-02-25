5a0f0c542df3fe102a6eb18d2ec3afec372a24b4
Alex <raposoalexander@gmail.com>
2025-02-25 05:02:27 -0500

    Refactored JWTProvider

---
+++ API/src/main/java/com/citycite_api/auth/service/JwtProvider.java
@@ -0,0 +1,66 @@
+package com.citycite_api.auth.service;
+
+import com.citycite_api.user.entity.AccountType;
+import io.jsonwebtoken.*;
+import jakarta.transaction.Transactional;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+@Service
+@Transactional
+@NoArgsConstructor
+public class JwtProvider implements iJwtProvider {
+
+    @Value("${app.jwt.secret}")
+    private String secretKey;
+
+    @Value("${app.jwt.access-token-expiration}")
+    private long expirationTimeMS;
+
+    @Override
+    public String generateAccessJWT(Integer userId, AccountType accountType) {
+        return Jwts.builder()
+                .setSubject(Integer.toString(userId))
+                .claim("role", accountType)
+                .setIssuer("CityCite")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMS))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();  // Create and return the token
+    }
+
+    @Override
+    public Claims getJWTClaims(String jwt) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    @Override
+    public Integer extractUserID(Claims jwtClaims) {
+        return Integer.parseInt(jwtClaims.getSubject());
+    }
+
+    @Override
+    public AccountType extractUserRole(Claims jwtClaims) {
+            return AccountType.valueOf(jwtClaims.get("role", String.class));
+    }
+
+    @Override
+    public boolean isExpired(Claims jwtClaims) {
+            Date expirationDate = jwtClaims.getExpiration();
+            return expirationDate.before(new Date());
+    }
+
+    @Override
+    public String removeBearerPrefix(String rawJwt) throws MalformedJwtException {
+        if (rawJwt == null || !rawJwt.startsWith("Bearer ")){
+            throw new MalformedJwtException("Invalid JWT provided!");
+        }
+        return rawJwt.substring(7);
+    }
+
+}
--- API/src/main/java/com/citycite_api/auth/service/iJwtProvider.java
+++ API/src/main/java/com/citycite_api/auth/service/iJwtProvider.java
@@ -2,16 +2,17 @@ package com.citycite_api.auth.service;
 
 import com.citycite_api.user.entity.AccountType;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
-public interface iAccessJWTService {
+public interface iJwtProvider {
     public String generateAccessJWT(Integer userId, AccountType accountType);
     public Claims getJWTClaims(String token);
-    public boolean isValidSignature(String token);
-    public boolean isExpired(String token);
-    public Integer extractUserID(String token);
-    public AccountType extractUserRole(String token);
+    public AccountType extractUserRole(Claims jwtClaims);
+    public Integer extractUserID(Claims jwtClaims);
+    public boolean isExpired(Claims jwtClaims);
+    public String removeBearerPrefix(String jwt) throws MalformedJwtException;
 }
