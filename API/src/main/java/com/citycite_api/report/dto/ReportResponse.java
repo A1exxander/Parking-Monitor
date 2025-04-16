afbd34f2fa004facb9aecba59c0a58224c65e74f
Alex <raposoalexander@gmail.com>
2025-04-16 12:46:56 -0400

    Added jakarta annotation validations to ReportResponse

--- API/src/main/java/com/citycite_api/report/dto/ReportResponse.java
+++ API/src/main/java/com/citycite_api/report/dto/ReportResponse.java
@@ -3,21 +3,49 @@ package com.citycite_api.report.dto;
 import com.citycite_api.enforcement.dto.OfficerResponse;
 import com.citycite_api.report.entity.ResolutionStatus;
 import com.citycite_api.user.dto.UserResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import java.time.Instant;
 
 @Data
 public class ReportResponse {
 
-    private Integer reportID;
+    @NotNull
+    @Min(1)
+    private Integer ID;
+
+    @NotNull
+    @Valid
     private UserResponse submittingUser;
+
+    @Null
+    @Size(max = 256)
     private String violationDescription;
+
+    @NotNull
+    @Valid
     private ReportAddressDTO reportAddress;
+
+    @NotNull
+    @Valid
     private ReportVehicleDTO reportVehicle;
+
+    @Null
     private OfficerResponse respondingOfficer;
+
+    @Null
     private ResolutionStatus resolutionStatus;
+
+    @Null
+    @Size(max = 64)
     private String resolutionNotes;
+
+    @NotNull
+    @Past
     private Instant createdAt;
+
+    @Null
     private Instant updatedAt;
 
 }
