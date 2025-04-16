54ed2cb646935c18422d52b3a5a2a3e4bf61baf1
Alex <raposoalexander@gmail.com>
2025-04-16 17:00:13 -0400

    Added a feature to get reports for the current officer or user

--- API/src/main/java/com/citycite_api/report/controller/ReportController.java
+++ API/src/main/java/com/citycite_api/report/controller/ReportController.java
@@ -1,10 +1,16 @@
 package com.citycite_api.report.controller;
 
 import com.citycite_api.report.dto.GeoReportResponse;
+import com.citycite_api.report.dto.ReportResponse;
 import com.citycite_api.report.service.iReportService;
+import com.citycite_api.user.entity.AccountType;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -33,5 +39,37 @@ public class ReportController implements iReportController {
         return ResponseEntity.ok(geoReportResponseList);
 
     }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<Page<ReportResponse>> getReports(@NotNull Authentication authentication,
+                                                           @PageableDefault(page = 0, size = 10, direction = Sort.Direction.DESC) Pageable pageable) {
+
+        // Extract authenticated user information
+        Integer userID = (Integer) authentication.getPrincipal();
+        AccountType accountType = AccountType.valueOf(authentication.getAuthorities()
+                .iterator()
+                .next()
+                .getAuthority()
+                .replaceFirst("^ROLE_", "")
+                .toUpperCase()); // Should prob move this into its own method, and use ROLE_ for account types in future
+
+        Page<ReportResponse> reportsResponse = null;
+
+        switch (accountType) {
+
+            case USER ->
+                    reportsResponse = reportService.getReportsWithSubmittingUserID(pageable, userID);
+            case OFFICER ->
+                    reportsResponse = reportService.getReportsWithRespondingOfficerID(pageable, userID);
+            default ->
+                    throw new IllegalArgumentException("Unsupported account type!");
+
+        };
+
+        return ResponseEntity.ok(reportsResponse);
+
+    }
+
 
 }
--- API/src/main/java/com/citycite_api/report/controller/iReportController.java
+++ API/src/main/java/com/citycite_api/report/controller/iReportController.java
@@ -1,12 +1,18 @@
 package com.citycite_api.report.controller;
 
 import com.citycite_api.report.dto.GeoReportResponse;
+import com.citycite_api.report.dto.ReportResponse;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,4 +23,8 @@ public interface iReportController {
     @PreAuthorize("hasRole('OFFICER')")
     public ResponseEntity<List<GeoReportResponse>> getAllGeoReports(@NotNull Authentication authentication);
 
+
+    @GetMapping
+    public ResponseEntity<Page<ReportResponse>> getReports(@NotNull Authentication authentication,
+                                                    @PageableDefault(page = 0, size = 10, direction = Sort.Direction.DESC) Pageable pageable);
 }
