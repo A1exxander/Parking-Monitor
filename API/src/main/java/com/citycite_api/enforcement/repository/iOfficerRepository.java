66843a2e83ee16fa289f65386ae4b11843b2df41
Alex <raposoalexander@gmail.com>
2025-04-18 19:14:31 -0400

    Removed unused and useless findBy method from officer repository

--- API/src/main/java/com/citycite_api/enforcement/repository/iOfficerRepository.java
+++ API/src/main/java/com/citycite_api/enforcement/repository/iOfficerRepository.java
@@ -2,10 +2,7 @@ package com.citycite_api.enforcement.repository;
 
 import com.citycite_api.enforcement.entity.Officer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface iOfficerRepository extends JpaRepository<Officer, Integer> {
-    @Query("SELECT o FROM Officer o WHERE o.id = :officerID")
-    Officer findOfficerById(@Param("officerID") Integer officerID);
-}
+@Repository
+public interface iOfficerRepository extends JpaRepository<Officer, Integer> {}
